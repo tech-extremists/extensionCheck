341bebf00943aa7aa81142129ff0b1431495740d
Atakan Şerifoğlu <atakan.serifoglu@ug.bilkent.edu.tr>
2024-09-28 12:51:24 +0300

    Create controller.java

---
+++ controller.java
@@ -0,0 +1,14 @@
+
+public class controller {
+    public static void main(String[] args) {
+        // Declare two numbers
+        int num1 = 10;
+        int num2 = 20;
+
+        // Perform addition
+        int sum = num1 + num2;
+
+        // Print the result
+        System.out.println("The sum of " + num1 + " and " + num2 + " is: " + sum);
+    }
+}
