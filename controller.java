b32447f96ee0803da85dc84cd7137b7895ea9a6a
Atakan Şerifoğlu <91365460+atakanserifoglu@users.noreply.github.com>
2024-10-04 16:04:45 +0300

    Update controller.java
    Co-authored-by: tech-extremists <techextremists24@gmail.com>

--- controller.java
+++ controller.java
@@ -3,7 +3,7 @@ public class controller {
     public static void main(String[] args) {
         // Declare two numbers
         int num1 = 20;
-        int num2 = 30;
+        int num2 = 40;
 
         // Perform addition
         int sum = num1 + num2;
