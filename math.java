3142e8c0c0a15d20d307c9f3c116a29b474c07c7
Deeployed <techextremists24@gmail.com>
2024-12-06 11:52:47 +0300

    Update math.java

--- math.java
+++ math.java
@@ -1,6 +1,7 @@
 public class math {
     public static int add(int num1, int num2) {
-        int result = num1 + num2;
+        int answer = num1 + num2;
+        console.log(answer);
         return ans;
     }
 }
