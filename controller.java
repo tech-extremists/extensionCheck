8b6dce7ae702c2bc59eca78d694eda78c5098389
Atakan Şerifoğlu <91365460+atakanserifoglu@users.noreply.github.com>
2024-12-18 18:46:45 +0300

    Update controller.java
    Co-authored-by: Deeployed <techextremists24@gmail.com>

--- controller.java
+++ controller.java
@@ -7,7 +7,7 @@ public class controller {
 
         // Perform addition
         int summation = math.add(num2, num1);
-        System.out.println("xofz");
+        System.out.println("xofsz");
         // Print the result
         System.out.println("The result of " + num1 + " and " + num2 + " is: " + sum);
     }
