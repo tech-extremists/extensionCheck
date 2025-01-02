682c9cf739f56019083b368481870cba2a1a784e
Serhan-Can <91366657+Serhan-Can@users.noreply.github.com>
2025-01-02 22:39:01 +0300

    Update math.java

--- math.java
+++ math.java
@@ -1,7 +1,7 @@
 public class math {
     public static int add(int num1, int num2) {
         int answer = 0;
-        int answer = num1 + num2;
-        return ans;
+        int result = num1 + num2;
+        return result;
     }
 }
